1a6c530c704c2e96791f0de431fca1736a24c771
nsakki55 <n.sakki55@gmail.com>
2019-06-09 19:53:17 +0900

    c++

---
+++ C++/grammer/type.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s="hello world";
+    printf("%s\n",s.c_str());
+
+    int number=100;
+    string str=to_string(number);
+
+    printf("%s\n",str.c_str());
+
+    string s_num="100";
+    int num=stoi(s_num);
+    printf("%d",num);
+
+
+}
