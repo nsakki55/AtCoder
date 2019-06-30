f93e9707d8d84922b7e4ff1a6a967cb698a026f3
nsakki55 <n.sakki55@gmail.com>
2019-06-30 12:49:16 +0900

    ABC132

--- C++/ABC132/A.cpp
+++ C++/ABC132/A.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    string S;
-    cin>>S;
-
-    cout<<S[0]<<endl;
+    string s;
+    cin>>s;
+    bool ans=false;
+    if(s[0]==s[1] && s[2]==s[3] && s[0] !=s[2]) ans=true;
+    if(s[0]==s[2] && s[1]==s[3] && s[0] !=s[1]) ans=true;
+    if(s[0]==s[3] && s[1]==s[2] && s[0] !=s[1]) ans=true;
+    if(ans) puts("Yes");
+    else puts("No");
 
     return 0;
 }
